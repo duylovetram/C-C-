fb84d0e694bff6bbdb1dcf316486110e78b920e3
duylovetram <156053816+duylovetram@users.noreply.github.com>
2024-04-11 14:52:50 +0700

    Update Source.cpp

--- Project10/Source.cpp
+++ Project10/Source.cpp
@@ -17,14 +17,3 @@ int main()
 			printf("%d\t", num);
 	}
 }
-
-int n;
-printf("nhap do dai canh: ");
-scanf("%d", &n);
-int i; j;
-for (i = 0; i < n; i++) {
-	for (j = 0; j < n; j++) {
-		printf("* ");
-	}
-	printf("\n");
-}
